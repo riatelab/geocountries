1a7abbffd12a64b8bd45c129a021547f8983b5c3
neocarto <nicolas.lambert@cnrs.fr>
2022-02-04 09:47:57 +0100

    update

--- src/getcode.js
+++ src/getcode.js
@@ -1,11 +1,12 @@
 import {similarity} from "./levenshtein.js"
 import {codes} from "./codes.js"
 
-export function getcode(str){
+export function getcode(str, threshold = 0.75){
   let result = [];
   codes.forEach((e) => {
     for (let i = 1; i < Object.keys(e).length; i++) {
       let sim = similarity(str, Object.values(e)[i]);
+
       result.push([Object.values(e)[0], sim]);
     }
   });
@@ -13,6 +14,8 @@ export function getcode(str){
   result = result.sort((a, b) => {
     return b[1] - a[1];
   })[0];
+
+  if (result[1] < threshold) {result = [undefined, 0]}
 
   return { name:str, iso3: result[0], score: result[1] };
 }
