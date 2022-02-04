ca984dc1b18576f6d942582b59ef54592cec0b3a
neocarto <nicolas.lambert@cnrs.fr>
2022-02-04 09:05:11 +0100

    fix threshold

--- src/iso3.js
+++ src/iso3.js
@@ -1,10 +1,12 @@
-import {getcode} from "./getcode.js"
-
-export function iso3(data, name){
+iso3 = function (data, name, threshold = 0.75) {
   const result = [];
   const names = Array.from(new Set(data.map((d) => d[name])));
   names.forEach((e) => {
-    result.push([e, getcode(e)]);
+    let r = geocountries.getcode(e);
+    if (r.score <= threshold) {
+      r = { name: e, iso3: undefined, score: 0 };
+    }
+    result.push([e, r]);
   });
   return new Map(result);
 }
