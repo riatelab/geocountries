7510e5560ff794800daf5a0448139a88c781eb10
neocarto <nicolas.lambert@cnrs.fr>
2022-11-09 10:54:22 +0100

    Fix : data in a csv file

--- src/getcode.js
+++ src/getcode.js
@@ -1,10 +1,12 @@
-import {similarity} from "./levenshtein.js"
-import {codes} from "./codes.js"
+import { similarity } from "./levenshtein.js";
+import { csv } from "d3-fetch";
+import { codes } from "./codes.js";
 
+const data = codes();
 
-export function getcode(str, threshold = 0.9){
+export function getcode(str, threshold = 0.9) {
   let result = [];
-  codes.forEach((e) => {
+  data.forEach((e) => {
     for (let i = 1; i < Object.keys(e).length; i++) {
       let sim = similarity(str, Object.values(e)[i]);
 
@@ -16,7 +18,9 @@ export function getcode(str, threshold = 0.9){
     return b[1] - a[1];
   })[0];
 
-  if (result[1] < threshold) {result = [undefined, 0]}
+  if (result[1] < threshold) {
+    result = [undefined, 0];
+  }
 
-  return { name:str, iso3: result[0], score: result[1] };
+  return { name: str, iso3: result[0], score: result[1] };
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-export {similarity} from "./levenshtein.js";
-export {getcode} from "./getcode.js";
-export {iso3} from "./iso3.js";
-export {view} from "./view.js";
-export {add} from "./add.js";
-export {addiso3} from "./addiso3.js";
-export {duplicate} from "./duplicate.js";
+export { similarity } from "./levenshtein.js";
+export { getcode } from "./getcode.js";
+export { iso3 } from "./iso3.js";
+export { view } from "./view.js";
+export { add } from "./add.js";
+export { addiso3 } from "./addiso3.js";
+export { duplicate } from "./duplicate.js";
+export { codes } from "./codes.js";
