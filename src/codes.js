ad3c9e9082edb7330008231b28f43a27d11b0637
neocarto <nicolas.lambert@cnrs.fr>
2022-11-09 10:55:04 +0100

    fix : data name

--- src/codes.js
+++ src/codes.js
@@ -1,7 +1,7 @@
 import { csv } from "d3-fetch";
 export async function codes() {
   const data = await csv(
-    "https://raw.githubusercontent.com/neocarto/geocountries/main/data/codes.js"
+    "https://raw.githubusercontent.com/neocarto/geocountries/main/data/codes.csv"
   );
   return data;
 }
